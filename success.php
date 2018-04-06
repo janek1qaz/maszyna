9af93880123c34fb3ed1b2222a8d8a3c68c8222d
janek1qaz <janek1qaz@gmail.com>
2018-04-06 17:14:18 +0200

    git

---
+++ success.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<?php
+    // Start the session
+    ob_start();
+    session_start();
+
+    // Check to see if actually logged in. If not, redirect to login page
+    if (!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] == false) {
+        header("Location: index.php");
+    }
+?>
+
+<html> 
+
+<head>
+	<style>
+	body{
+		background-color: rgba(0,0,0,1.00);
+		color:white;
+    background-size: cover; background-repeat: no-repeat; margin-top:0; 
+		
+	margin:0px;
+	padding:4px:
+		}
+div{
+	overflow:hidden;}
+	#ramka{
+			font-family:Arial;
+			background-image: url();
+		text-align: center;
+		width:100%;
+		margin-right:auto;
+		
+	margin-left:auto;}
+	#naglowek{
+		font-family:Arial;
+	
+		width:100%;
+		padding-bottom: 10px;
+		height:106px;
+		background-image: url(galamshop.png);
+		background-repeat: no-repeat;
+		background-position: center;
+		font-size:85px;
+		text-align:center;		border-bottom: solid thin rgba(255,255,255,1.00);
+	background-color:#000000;}
+	#stopka{
+
+		width:100%;
+		height:49px;
+				border-bottom: solid thin rgba(255,255,255,1.00);
+		border-top: solid thin rgba(255,255,255,1.00);
+		font-size:12px;
+		letter-spacing:5px;
+		padding-top:3px;
+		}
+	
+	#kol1{
+		color:white;
+		 box-sizing: border-box;
+		padding-top: 35px;
+			border-right: solid thin rgba(255,255,255,1.00);
+		
+		float:left;
+		font-family:Arial;
+		font-weight: bold;
+		font-size:21px;
+		height:800px;
+		width:300px;
+line-height:240px;
+		
+	background-color: #000000}
+		.id1 {
+			color:white
+		}
+			.id1:hover {
+			color:red;
+		
+		}
+		img:hover {
+			width:100%;
+			background-color:yellow;
+		}
+		#kol2{
+		
+		   box-sizing: border-box;
+		text-align: center;
+		font-family:Arial;
+		
+		font-size:27px;
+		height:800px;
+	width:73.725%;
+				border-left: solid thin rgba(255,255,255,1.00);
+		
+		float:left;}
+		a {
+			
+		}
+	#kol3{
+		
+		   box-sizing: border-box;
+		text-align: center;
+		font-family:Arial;
+		font-weight: bold;
+		padding-top:20px;
+		font-size:29px;
+		height:800px;
+	width:300px;
+		padding:15px;
+				border-left: solid thin rgba(255,255,255,1.00);
+	
+		float:right;
+		}
+	</style>
+	<title>GALAMSHOP</title>
+</head>
+<body>
+<div id="ramka">
+<a href="index.php"><div id="naglowek"></div></a>
+
+	<div id="kol1">
+	<a class="id1" style="text-decoration: none;" href="stronagl.php">STRONA GŁÓWNA</a>
+	<a class="id1" style="text-decoration: none;" href="logclient.php">ZALOGOWANY KLIENT</a>
+	<a class="id1" style="text-decoration: none;" href="contact.php">KONTAKT</a>
+	</div>
+	<div id="kol3">
+	NOWOŚCI<br><br><br>
+		<img width="170px" alt="laptop1" src="laptop1.png"><br><br><br><br><br>
+		<img width="170px" alt="laptop2" src="laptop2.png"><br><br><br><br><br>
+		<img width="170px" alt="laptop3" src="laptop3.png">
+	</div>
+	
+
+
+	
+<div id="stopka"><font face="Arial Light" size="2">ADOBE DREAMWEAVER<b style="font-weight:bold; font-size:20px;">&amp;</b>AFTER EFFECTS<br><b style="font-weight:bold; font-size:20px;">03.04.18r.</b></font></div>
+</div>
+
+	
+<h1>Logged In!</h1>
+<form method="post" action="logout.php">
+    <input type="submit" value="Logout">
+</form>
+</body>
+</html>
